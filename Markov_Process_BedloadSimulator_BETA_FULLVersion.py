1a8efa5ced78cec9a24d05348c3fed8073ed1935
Shawn Chartrand <shawn.chartrand@geog.ubc.ca>
2018-06-21 22:53:49 -0700

    Building part 2 of the code.

--- Markov_Process_BedloadSimulator_BETA_FULLVersion.py
+++ Markov_Process_BedloadSimulator_BETA_FULLVersion.py
@@ -7,32 +7,30 @@ import math
 import random
 import time
 
-import matplotlib
-import matplotlib.collections
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Circle
-
-# First two lines clear variables from memory each time the script is executed.
-# Import the necessary libraries
-# from IPython import get_ipython
-# get_ipython().magic('reset -sf')
+import matplotlib.cm as cm
 
 start_time = time.time()
+#
 # BEGIN CODE
 # This code was developed by Shawn Chartrand and David Furbish, March 2018.
 # The code randomly populates a rectangular area of specified dimension with
-# circles of random and nonuniform size. The range of circle sizes is
-# is specified through a user interface. Placement of circles at step S
+# circles of random and nonuniform size. Placement of circles at step S
 # does not depend on prior placements. Placement of circles stops when a
 # specified packing fraction (circle volume/total volume) is reached.
-# A maximum packing fraction of 70% is generally specified. The code is the
-# basis of Markov birth-death simulations of bedload transport in rivers
-# under low transport rates.
+# The code is the basis of Markov birth-death simulations of bedload transport
+# in rivers under relatively low transport rates. The code is split into two
+# parts. Part 1 builds the random streambed surface. Part 2 entrains, deposits
+# and moves particles along the streambed surface.
 ###############################################################################
 ###############################################################################
-# STEPS ONE, TWO AND THREE DEFINE VARIABLES AND BUILD THE MODEL DOMAIN.
+# PART 1
+#
+# STEPS ONE, TWO AND THREE: DEFINE VARIABLES AND BUILD THE MODEL DOMAIN.
+#
 # STEP ONE: DEFINE DATA STRUCTURES, INITIALIZE SOME VARIABLES.
 # DATA LISTS
 # Circle area.
@@ -60,18 +58,21 @@ theta = np.arange(0, (2 * math.pi), 0.01)
 XCoordinates_Orig = 0
 YCoordinates_Orig = 0
 # END SECTION
+#
 # STEP TWO: BUILD THE VIRTUAL DOMAIN WITH ALL NECESSARY DETAILS
 # KEY INPUT PARAMETER VALUES
 # Packing density of the bed; ranges from > 0 to <~0.70
 Pack = 0.52
 # Length of the domain in the streamwise direction in millimeters
-x_max = 2000
+x_max = 200
 # Length of the domain in the cross-stream direction in millimeters
 y_max = 100
 # Spacing nodes
 Grid_Spacing = 2.0
 # Size of largest grain relative to the cross-stream length
 Size_Frac = 0.06
+#Time threshold to restart script
+time_threshold = 10
 # DEPENDENT CALCULATIONS
 # Total virtual streambed area.
 Total_VArea = x_max * y_max
@@ -83,6 +84,7 @@ CircDiam_Fac = 1 / Size_Frac
 # Specify the max circle diameter.
 Max_CircleDiameter = y_max / CircDiam_Fac
 # END SECTION
+#
 # STEP THREE: BUILD MATRIX OF COORDINATE ADDRESSES FOR CENTER OF CIRCLES
 # Build x array for grid.
 x_array = np.arange(x_min_incre, (x_max+1), x_min_incre, dtype=float)
@@ -106,7 +108,7 @@ XCoordinates = np.repeat(x_array[None], length1, axis=0)
 # END SECTION
 ###############################################################################
 ###############################################################################
-# STEP FOUR: DEFINE FUNCTION USED TO PLACE CIRCLES
+# STEP FOUR: DEFINE TWO FUNCTIONS USED TO PLACE and REVISE CIRCLE DIAMETERS
 
 
 def fu_radcheck(x_Center, y_Center, radius):
@@ -137,8 +139,44 @@ def fu_radcheck(x_Center, y_Center, radius):
     y1 = y_Center - radius
     y2 = y1 + (radius * 2)
     coord = [x1, x2, y1, y2]
-    return radius, coord
-# END SECTION
+    if step == 1:
+        newCircle_Found = 1 
+    else:
+        newCircle_Found = 0 
+    return radius, coord, newCircle_Found
+
+def fu_diamrevise(x_Center, y_Center, radius):
+    # The following operations determines if the test circle overlaps
+    # with existing circles, but only to the resolution of the
+    # underlying grid. The outcome is a logical matrix. Values of 1
+    # mean that grid point overalps between the test circle and an
+    # existing circle.
+    dist_check = ((CenterCoordinates[0, :] - x_Center) ** 2 +
+                  (CenterCoordinates[1, :] - y_Center) ** 2) ** 0.5
+    radius_check = (Diameter / 2) + radius
+    radius_overlap = (radius_check >= dist_check)
+    # Now actually check the overalp condition.
+    if np.sum([radius_overlap]) == 0:
+        # The new circle does not overlap so proceed.
+        newCircle_Found = 1
+    elif np.sum([radius_overlap]) == 1:
+        # The new circle overlaps with one other circle
+        overlap = (np.arange(0, len(radius_overlap[0]),
+                             dtype=int).reshape(1, len(radius_overlap[0])))
+        overlap_logix = (radius_overlap == 1)
+        idx_true = overlap[overlap_logix]
+        radius = dist_check[idx_true] - (Diameter[0, idx_true] / 2)
+        # New circle radius can be negative when inside another circle.
+        if radius > 0:
+            newCircle_Found = 1
+        else:
+            newCircle_Found = 0
+    elif np.sum([radius_overlap]) >= 2:
+        # Overlap with two or more circles.
+        # Set newCircle flag to zero so the step is repeated.
+        newCircle_Found = 0
+    return radius, newCircle_Found
+ # END SECTION
 ###############################################################################
 ###############################################################################
 # STEP FIVE: RANDOMLY PLACE CIRCLES IN RECTANGULAR AREA
@@ -172,40 +210,13 @@ while (AreaTotal / Total_VArea) < Pack:
     CenterPosition_data = [x_Center, y_Center]
     radius = diameter_Test / 2
     # CALL FUNCTION
-    radius, coord = fu_radcheck(x_Center, y_Center, radius)
+    radius, coord, newCircle_Found = fu_radcheck(x_Center, y_Center, radius)
     # Check to see if radius needs revision for steps greater than the first
     if step != 1:
-        # The following operations determines if the test circle overlaps
-        # with existing circles, but only to the resolution of the
-        # underlying grid. The outcome is a logical matrix. Values of 1
-        # mean that grid point overalps between the test circle and an
-        # existing circle.
-        dist_check = ((CenterCoordinates[0, :] - x_Center) ** 2 +
-                      (CenterCoordinates[1, :] - y_Center) ** 2) ** 0.5
-        radius_check = (Diameter / 2) + radius
-        radius_overlap = (radius_check >= dist_check)
-        # Now actually check the overalp condition.
-        if np.sum([radius_overlap]) == 0:
-            # The new circle does not overlap so proceed.
-            newCircle_Found = 1
-        elif np.sum([radius_overlap]) == 1:
-            # The new circle overlaps with one other circle
-            overlap = (np.arange(0, len(radius_overlap[0]),
-                                 dtype=int).reshape(1, len(radius_overlap[0])))
-            overlap_logix = (radius_overlap == 1)
-            idx_true = overlap[overlap_logix]
-            radius = dist_check[idx_true] - (Diameter[0, idx_true] / 2)
-            # New circle radius can be negative when inside another circle.
-            if radius > 0:
-                newCircle_Found = 1
-            else:
-                newCircle_Found = 0
-        elif np.sum([radius_overlap]) >= 2:
-            # Overlap with two or more circles.
-            # Set newCircle flag to zero so the step is repeated.
-            newCircle_Found = 0
+        # CALL FUNCTION
+        radius, newCircle_Found = fu_diamrevise(x_Center, y_Center, radius)    
     # Only write data if a circle was placed.
-    if step == 1 or newCircle_Found == 1:
+    if newCircle_Found == 1:
         # Write data to variables
         # coord = np.array([x1, x2, y1, y2])
         # This function moves circles in a rectangle to available grid points
@@ -234,11 +245,35 @@ while (AreaTotal / Total_VArea) < Pack:
         while len(milestones) > 0 and percentage_complete >= milestones[0]:
             print "{}% complete".format(milestones[0])
             # remove that milestone from the list
-            milestones = milestones[1:]
+            milestones = milestones[1:]       
+    # This operation restarts the script if the execution time is too long.
+    # I think there is a bug but I cannot figure out what it is. Filling the
+    # rectangular area stalls out at 95 or 99% about 1 out of every 4 runs.
+    current_time = time.time()
+    if (current_time - start_time) > time_threshold:
+        start_time = time.time()
+        step = 1
+        percentage_complete = 0
+        # Circle area.
+        Area = np.zeros([1, 1], dtype=int, order='F')
+        # Total area of placed circles. Initialize to 0.
+        AreaTotal = 0
+        # Coordinates of placed circles.
+        Coordinates = np.zeros([4, 1], dtype=int, order='F')
+        # Center coord. of placed circles: r1:x,r2:y.
+        CenterCoordinates = np.zeros([2, 1], dtype=int, order='F')
+        # Final placed circle diameters.
+        Diameter = np.zeros([1, 1], dtype=int, order='F')
+        # For logical mask.
+        XYCoordinateslogical = np.zeros((length1, length2), dtype=int)
+        # Milestones used to display calculation progress.
+        milestones = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99, 100]
+        print("--- RESTARTING LOOP ---")
+            
 # END SECTION
 ###############################################################################
 ###############################################################################
-# STEP SIX PRINT RESULTS TO SCREEN AND WRITE PERTINENT RESULTS TO FILE
+# STEP SIX: PRINT RESULTS TO SCREEN AND WRITE PERTINENT RESULTS TO FILE
 print("--- %s seconds ---" % (time.time() - start_time))
 # STEP SIX: PLOT THE RESULTS
 fig = plt.figure(1)
@@ -260,19 +295,22 @@ for x1, y1, r in zip(XCenter, YCenter, Radius_Array):
     circle = Circle((x1, y1), r)
     patches.append(circle)
 colors = 1000*np.random.rand(len(CenterCoordinates[0]))
-p = (PatchCollection(patches, cmap=matplotlib.cm.gist_gray,
+p = (PatchCollection(patches, cmap=cm.binary,
                      alpha=0.9, linewidths=(0, )))
 p.set_array(colors)
 p.set_clim([5, 950])
 ax.add_collection(p)
 plt.show()
-fig.savefig('Circles_test.pdf', format='pdf', dpi=2400)
+# Need to insert some commands to write files to subdirectory
+fig.savefig('Starting_Bed.pdf', format='pdf', dpi=2400)
 # Save files for 3D plotting
 np.save('XCenter', XCenter)
 np.save('YCenter', YCenter)
 np.save('Radius_Array', Radius_Array)
-# END SECTION
+# END OF PART 1 VIRTUAL BED CREATION
 ###############################################################################
 ###############################################################################
-
+# PART 
+#
+# STEP ONE:
 # END OF CODE
